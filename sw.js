f12c2f5f0051ab0bf8dff7a93da2cc5c53a50d7a
enoughsleep <100878986+enoughsleep@users.noreply.github.com>
2022-03-03 14:23:36 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e =>{
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css", "./images/logo192.png","./src/script.js"])
+            return cache.addAll(["./", "./src/master.css", "./images/logo192.png","./src/script.js","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css","https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"])
         })
     );
 });
